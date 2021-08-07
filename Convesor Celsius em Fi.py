4a50b45f4ad6b8c5183a4289426bef52cd735d05
Gabriel Ricardo <gabrielricardo2803@hotmail.com>
2021-08-07 20:07:44 -0300

    Primeiro commit

---
+++ Convesor Celsius em Fi.py
@@ -0,0 +1,14 @@
+﻿import time
+import os
+
+print ("CONVERSOR DE TEMPERATURA °C PARA °F")
+time.sleep(4)
+os.system ("cls")
+print("By Gabriel Ricardo =)")
+time.sleep(4)
+os.system ("cls")
+tempc = float(input ("Informe sua Temperatura em °C: "))
+f= ((9*tempc)/5)+32
+print ("{}°C em Celsius é {}°F em Fahrenheit".format(tempc, f))
+time.sleep (4)
+os.system ("cls")
